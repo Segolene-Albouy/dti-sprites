6a44468af89fa5d48d8956f9362c1e721e1d8898
Sonat Baltaci <sonat.baltaci@gmail.com>
2023-11-01 14:54:37 +0100

    Add seed to sh

--- sprites-pipeline.sh
+++ sprites-pipeline.sh
@@ -2,4 +2,7 @@ set -e
 CURRENTDATE=`date +"%m%d"`
 run_tag="${CURRENTDATE}_$tag"
 dataset="$(sed -n 2p configs/$config | cut -f2 -d ':' | cut -c2-)"
+
+seed=$(shuf -i 1-10000 -n 1)
+sed -i "s/seed:.*/seed: $seed/" configs/$config
 CUDA_VISIBLE_DEVICES=$cuda python src/sprites_trainer.py --tag $run_tag --config $config
