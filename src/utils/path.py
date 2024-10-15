94c48f2accc0391d9e47d9bb69f3655824818140
Sonat Baltaci <sonat.baltaci@gmail.com>
2024-10-15 10:03:36 +0200

    add new configs

--- src/utils/path.py
+++ src/utils/path.py
@@ -8,7 +8,9 @@ DEFAULT_PROJECT_PATH = Path(__file__).parent.parent.parent
 ENV = Env()
 PROJECT_PATH = Path(ENV("DTI_DATA_FOLDER", default=DEFAULT_PROJECT_PATH))
 
+DATASET_SCRATCH = Path('/lustre/fsn1/projects/rech/wgc/uze77wm')
+
 CONFIGS_PATH = PROJECT_PATH / 'configs'
-DATASETS_PATH = PROJECT_PATH / 'datasets'
+DATASETS_PATH = DATASET_SCRATCH / 'datasets' # PROJECT_PATH / 'datasets'
 RUNS_PATH = PROJECT_PATH / 'runs'
 RESULTS_PATH = PROJECT_PATH / 'results' # unused?
